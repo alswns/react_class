dd47b9feb7a19609dd4cdc06635bff5f8a8bbdd2
alswns <49472611+alswns@users.noreply.github.com>
2019-11-07 10:35:22 +0900

    google

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,36 @@ import './App.scss';
 
 function App() {
   return (
-   <div>
-    <div className="root">
-    <header>head</header>
-    <nav>nav</nav>
-    <div>
-    <section>main</section>
-    <aside>aside</aside>
+   <div className='root'>
+    <div className="top">
+    <a href="https://mail.google.com/mail/?tab=rm&ogbl">Gmail </a>
+    <a href="https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl">이미지 </a>
+    <a href="" ><img src={require('./button.png')} alt=""/> </a>
+    <a href=""><img src={require('./icon.png')} alt=""/> </a>
+    </div>
+
+    <div className="mid">
+    
+      <img src={require('./google.png')} width={272} height={92} className='google'></img>
     </div>
     
-    <footer>footer</footer>
+    <div className="middle">
+      
+      <img src={require("./돋보기.png")} height={44} className='img'/>
+    <input placeholder="Google 검색 또는 URL입력" id="input" src={require('./search.png')}></input>
+    <a href=""  className='mike'><img src={require("./mike.png")} height={44}/></a>
+    </div>
     
     
+    <div className="bot">
+      <a href="https://www.naver.com"><img src={require("./naver.png")} alt=""/></a>
+
+
     </div>
     </div>
+    
+   
+   
  
   );
 }
