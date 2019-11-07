3ac8a38d172d191956305e10f465ed9b38eb48f0
alswns <49472611+alswns@users.noreply.github.com>
2019-11-07 21:11:01 +0900

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,73 @@
 import React from 'react';
-import './App.scss';
+import styled,{keyframes} from "styled-components";
+const ShowDiv=styled.div`
+display:block;
+`;
+
+// const Root=styled.a`
+//   border: 1px solid red;
+//   color:${props=>props.color};
+//   ${props=>props.show||css`
+//   opacity:0%;
+//   `}  
+//   /* margin: ${props=>props.margin}px;  */
+// `
+// const NoShowDiv=styled.div`
+// display:none;
+// `
+const move=keyframes`
+from{
+  opacity:0;
+}
+to{
+  opacity:1;
+}
+`;
+
+const noMove=keyframes`
+from{
+  opacity:1;
+}
+to{
+  opacity:0;
+}
+`;
+
+const Img=styled.img`
+animation:${props=>(props.show? move:noMove)} 1s linear infinite;
+`;
+
+const Wrapper = styled.div`
+ padding : 15px;
+ display : flex;
+ align-items:center;
+ 
+ label {
+     font-size : 20px;
+     margin-left:10px;
+ }
+
+ div {
+     display:none;
+ }
+`;
+const Input=styled.input`
+&~div{
+  display: ${props => props.checked?"block":"none"};
+}
+`
 
 function App() {
-  return (
-   <div className='root'>
-    <div className="top">
-    <a href="https://mail.google.com/mail/?tab=rm&ogbl">Gmail </a>
-    <a href="https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl">이미지 </a>
-    <a href="" ><img src={require('./button.png')} alt=""/> </a>
-    <a href=""><img src={require('./icon.png')} alt=""/> </a>
-    </div>
-
-    <div className="mid">
-    
-      <img src={require('./google.png')} width={272} height={92} className='google'></img>
-    </div>
-    
-    <div className="middle">
-    
-      <img src={require("./돋보기.png")} height={44} className='img'/>
-    <input placeholder="Google 검색 또는 URL입력" id="input" src={require('./search.png')}></input>
-    <a href="" className='mike'><img src={require("./keyboard.png")} alt="" /> </a>
-    <a href=""  ><img src={require("./mike.png")  } height={44}/></a>
-
-    </div>
-    
-    
-    <div className="bot">
-      <a href="">Google 검색</a>
-      <a href="">I'm Feeling Lucky</a>
-
-
-    </div>
-
-    <div className="foot">
-    <span className='country'>대한민국</span> <br></br>
-    <div className='원딜'>
-    <div id='ad'>
-    <a href=""  >광고</a>
-    <a href="">비즈니스</a>
-    <a href="">Google정보</a>
-    <a href="">검색의 원리</a>
-    </div>
-    <div>
-    <a href="" className='asdf'>개인정보처리방침</a><a href="">약관</a> <a href="">설정</a>
-    </div>
-    </div>
-    </div>
-    </div>
-   
-   
- 
-  );
+  const checked=false
+    return (
+        <Wrapper> 
+     <Input type="checkbox" id="check1" checked={checked}/>
+     <label htmlFor="check1">체크해봐</label>
+     <div>체크</div>
+    </Wrapper>
+    );   
 }
+
+
 
 export default App;
