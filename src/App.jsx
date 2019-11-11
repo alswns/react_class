feab7a65378aa2e416cb66d1eddd1ff61151bb7f
alswns <49472611+alswns@users.noreply.github.com>
2019-11-11 23:17:06 +0900

    sleep

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,47 @@
-import React from 'react';
-import styled,{keyframes} from "styled-components";
-const ShowDiv=styled.div`
-display:block;
-`;
+import React from "react";
+import TodoItem from "./TodoItem";
+ import "./App.css";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-// const Root=styled.a`
-//   border: 1px solid red;
-//   color:${props=>props.color};
-//   ${props=>props.show||css`
-//   opacity:0%;
-//   `}  
-//   /* margin: ${props=>props.margin}px;  */
-// `
-// const NoShowDiv=styled.div`
-// display:none;
-// `
-const move=keyframes`
-from{
-  opacity:0;
-}
-to{
-  opacity:1;
-}
-`;
+    this.state = {
+      items: [],
+      value: ""
+    };
+  }
 
-const noMove=keyframes`
-from{
-  opacity:1;
-}
-to{
-  opacity:0;
-}
-`;
+  onClickAdd = e => {
+    const items = this.state.items;
+    items.push(this.state.value);
+    this.setState({
+      items
+    });
+  };
 
-const Img=styled.img`
-animation:${props=>(props.show? move:noMove)} 1s linear infinite;
-`;
+  onChange = e => {
+    this.setState({ value: e.target.value });
+  };
 
-const Wrapper = styled.div`
- padding : 15px;
- display : flex;
- align-items:center;
- 
- label {
-     font-size : 20px;
-     margin-left:10px;
- }
-
- div {
-     display:none;
- }
-`;
-const Input=styled.input`
-&~div{
-  display: ${props => props.checked?"block":"none"};
-}
-`
-
-function App() {
-  const checked=false
+  render() {
     return (
-        <Wrapper> 
-     <Input type="checkbox" id="check1" checked={checked}/>
-     <label htmlFor="check1">체크해봐</label>
-     <div>체크</div>
-    </Wrapper>
-    );   
+      <div id='as'>
+      <div id='all'>
+    <span id='today'><h1>오늘 할 일</h1></span>
+        <div id='main'><input type='search' onChange={this.onChange} /> 
+        <button onClick={this.onClickAdd}>추가</button> </div>
+        
+
+        <div id='over'>
+        {this.state.items.map((value, idx) => (
+          <div className='items'>
+          <TodoItem key={idx} value={value} />
+          </div>
+          ))}
+          </div>
+      </div>
+      </div>
+    );
+  }
 }
-
-
-
 export default App;
